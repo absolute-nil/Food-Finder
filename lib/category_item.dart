61266a7cbccaa0dd6e08162bf2752413ffbbf2e1
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-07-30 12:50:51 +0530

    add navigation to meals screen on tap

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,20 +1,36 @@
 import 'package:flutter/material.dart';
+import './category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String _title;
   final Color _color;
   CategoryItem(this._title, this._color);
+
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+      return CategoryMealsScreen();
+    }));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Text(_title, style: Theme.of(context).textTheme.headline6,),
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-              colors: [_color.withOpacity(0.7), _color],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight),
-          borderRadius: BorderRadius.circular(20)),
+    return InkWell(
+      onTap: () => _selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(20),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(
+          _title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [_color.withOpacity(0.7), _color],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight),
+            borderRadius: BorderRadius.circular(20)),
+      ),
     );
   }
 }
