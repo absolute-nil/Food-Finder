9725045d6484fc2c3264e6fce663e69d450b906b
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-07-30 23:47:19 +0530

    Add Meal Widget and Refactor Files

---
+++ lib/widgets/meal_item.dart
@@ -0,0 +1,127 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import '../models/meal.dart';
+
+class MealItem extends StatelessWidget {
+  final String title;
+  final String imageUrl;
+  final Affordability affordability;
+  final Complexity complexity;
+  final int duration;
+
+  MealItem(
+      {@required this.title,
+      @required this.imageUrl,
+      @required this.duration,
+      @required this.affordability,
+      @required this.complexity});
+
+  void _selectMeal() {}
+
+  String get affordabilityText {
+    switch (affordability) {
+      case Affordability.Affordable:
+        return "affordable";
+        break;
+      case Affordability.Pricey:
+        return "pricey";
+        break;
+      case Affordability.Luxurious:
+        return "luxurious";
+        break;
+      default:
+      return "";
+    }
+  }
+
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.Simple:
+        return "simple";
+        break;
+      case Complexity.Challenging:
+        return "challenging";
+        break;
+      case Complexity.Hard:
+        return "hard";
+        break;
+      default:
+      return "";
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: _selectMeal,
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        elevation: 4,
+        margin: EdgeInsets.all(10),
+        child: Column(children: <Widget>[
+          Stack(
+            children: <Widget>[
+              ClipRRect(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10)),
+                child: Image.network(
+                  imageUrl,
+                  width: double.infinity,
+                  height: 250,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Positioned(
+                bottom: 20,
+                right: 10,
+                child: Container(
+                  width: 300,
+                  color: Colors.black54,
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  child: Text(
+                    title,
+                    style: TextStyle(color: Colors.white, fontSize: 26),
+                    softWrap: true,
+                    overflow: TextOverflow.fade,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.schedule),
+                    SizedBox(width: 6,),
+                    Text('$duration min'),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.work),
+                    SizedBox(width: 6,),
+                    Text(complexityText),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.attach_money),
+                    SizedBox(width: 6,),
+                    Text(affordabilityText),
+                  ],
+                )
+              ],
+            ),
+          )
+        ]),
+      ),
+    );
+  }
+}
